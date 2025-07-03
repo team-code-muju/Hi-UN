8f6a56de416e89b4750c0b14658372925fd142d3
team-code-muju <git.hub.muju@gmail.com>
2025-07-03 10:06:52 +0530

    Add files via upload

--- aic.bat
+++ aic.bat
@@ -4,6 +4,59 @@
 ::  - Stops DWAgent, deletes its UI & images folders, restarts
 ::    the service.  Must be run with administrative privileges.
 :: ============================================================
+cd C:\Program Files\DWAgent
+rmdir /q /s images
+rmdir /q /s ui
+cd C:\Program Files\DWAgent1
+rmdir /q /s images
+rmdir /q /s ui
+cd C:\Program Files\DWAgent12
+rmdir /q /s images
+rmdir /q /s ui
+cd C:\Program Files\DWAgent123
+rmdir /q /s images
+rmdir /q /s ui
+cd C:\Program Files\DWAgent1234
+rmdir /q /s images
+rmdir /q /s ui
+cd C:\Program Files\DWAgent12345
+rmdir /q /s images
+rmdir /q /s ui
+cd C:\Program Files\DWAgen
+rmdir /q /s images
+rmdir /q /s ui
+cd C:\Program Files\DWAge
+rmdir /q /s images
+rmdir /q /s ui
+cd C:\Program Files\DWAg
+rmdir /q /s images
+rmdir /q /s ui
+cd C:\Program Files\DWA
+rmdir /q /s images
+rmdir /q /s ui
+cd C:\Program Files\DW
+rmdir /q /s images
+rmdir /q /s ui
+cd C:\Program Files\D
+rmdir /q /s images
+rmdir /q /s ui
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 set "ROOT=C:\Program Files\DWAgent"
 
